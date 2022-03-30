9dd07ffce477840ba470f030cc167212b349ce5b
Victoria Arbaoui <arbaouivictoria@gmail.com>
2022-03-30 03:21:35 +0200

    mistake routz auteur

--- routes/auteur.js
+++ routes/auteur.js
@@ -7,7 +7,7 @@ const auth = require("../middleware/auth");
 Router.get('/', auteurCtrl.getAllAuteur);
 Router.post('/', auth, auteurCtrl.insertAuteur);
 Router.put('/', auth, auteurCtrl.putAuteur);
-Router.put('/:idauteur', auteurCtrl.putAuteurById)
+Router.put('/:idauteur', auth, auteurCtrl.putAuteurById)
 Router.delete('/:idauteur', auth, auteurCtrl.deleteAuteur);
 Router.delete('/nom/:nom', auteurCtrl.deleteByName);
 Router.get('/:idauteur', auteurCtrl.getById)
